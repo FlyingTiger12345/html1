a540112aa844a41b6fada01e34be4433f82a874f
FlyingTiger12345 <matthewvasquez1434@gmail.com>
2025-11-28 20:10:33 +0800

    Valid

---
+++ class21/class 23/script.js
@@ -0,0 +1,29 @@
+function clearScreen()
+{
+    document.getElementById("result").value = "";
+
+}
+
+function setScreenValue(value){
+    document.getElementById("result").value+=value
+
+}
+function calculateResult()
+{
+    const resultElement = document.getElementById("result");
+    const exp = resultElement.value.trim();
+    if (exp=="")
+    {
+        resultElement.value = "enter an experession"
+    
+    }
+    else
+    {
+        try{
+            resultElement.value = eval(exp)
+        }
+        catch(e){
+            resultElement.value = "invalid expression"
+        }
+    }
+}
